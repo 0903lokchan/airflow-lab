e6f2dd22c0abe48f4142f3558b3d005468d82958
Lok Chan <0903lokchan@gmail.com>
2024-10-28 09:52:55 +1300

    Fix test-dag Cloud Build trigger

--- airflow-environment.tf
+++ airflow-environment.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source = "hashicorp/google-beta"
+      version = "~> 6.8.0"
+    }
+  }
+}
+
 provider "google-beta" {
   project = "airflow-lab-439902"
   region  = "us-central1"
@@ -46,17 +55,53 @@ resource "google_composer_environment" "airflow_lab_dev" {
   }
 }
 
-resource "google_cloudbuild_trigger" "test-dags" {
-  name     = "test-dags"
+# Currently creating connections from Terraform is not supported?
+# resource "google_cloudbuildv2_connection" "github-connection" {
+#   name = "github-connection"
+#   project  = "airflow-lab-439902"
+#   location = "us-central1"
+
+#   github_config {
+#     authorizer_credential {
+#       oauth_token_secret_version = "projects/352517100677/secrets/github-connection-github-oauthtoken-d6231b/versions/1"
+#     }
+#   }
+# }
+
+resource "google_cloudbuildv2_repository" "gihub-repo" {
+  provider = google-beta
+  name = "airflow-lab"
   project  = "airflow-lab-439902"
   location = "us-central1"
-  github {
-    name = "airflow-lab"
-    owner = "0903lokchan"
+  parent_connection = "github-connection"
+  remote_uri = "https://github.com/0903lokchan/airflow-lab.git"
+}
+
+# /generate create a service account suitable for Cloud Build
+resource "google_service_account" "cloudbuild-sa" {
+  provider = google-beta
+  account_id = "cloudbuild-sa"
+  display_name = "Cloud Build Service Account"
+  project = "airflow-lab-439902"
+
+  disabled = false
+}
+
+
+resource "google_cloudbuild_trigger" "test-dags" {
+  provider = google-beta
+  name     = "test-dags"
+  description = "Perform unit tests on the DAGs before they are deployed to Composer."
+  project  = "airflow-lab-439902"
+  location = "us-central1"
+
+  repository_event_config {
+    repository = google_cloudbuildv2_repository.gihub-repo.id
     pull_request {
       branch = "^main$"
-      comment_control = "COMMENTS_ENABLED"
+      comment_control = "COMMENTS_ENABLED_FOR_EXTERNAL_CONTRIBUTORS_ONLY"
     }
   }
   filename = "/cloud_builds/test-dags.cloudbuild.yaml"
+  service_account = google_service_account.cloudbuild-sa.id
 }
